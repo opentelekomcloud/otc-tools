9a86317d2ccd72c35cbf94ec19e0138b86026ca8
Kurt Garloff <kurt@garloff.de>
2016-05-13 13:06:01 +0200

    otc-tools-v0.6.11:
    New command line options for VM (ECS) creation:
      --wait/--nowait, --volumetype, --bandwidth
    Defaults for VOLUMETYPE
    Derive AZ from subnet's AZ and check consistency
    Default count to 1 if nothing specified in ECS creation
    Optional --soft/--hard for ECS actions (reboot etc.)
    Document debug option in help message
    Only warn if s3 is missing, don't abort
    Fix otc ecs job <TASKID>

--- .otc_env.sh
+++ .otc_env.sh
@@ -1,9 +1,32 @@
-# == Module: Open Telekom Cloud Cli Interface Configuration 0.6
+# == Module: Open Telekom Cloud Cli Interface Configuration 0.6.x
 #
 # Store OTC Command Line Configuration
 #
+# Zsolt Nagy <Z.Nagy@t-systems.com>
+#
+# === Copyright
+#
+# Copyright 2016 T-Systems International GmbH
+#
+# USER SPECIFIC SETTINGS #################################################################
+
+# NEEDED FOR TOKEN AUTH
+# If you are using the otc tool along with native openstack client tools
+# just put the normal OS_ variables in novarc or .ostackrc and you're good to go
+
+test -e ~/novarc && source ~/novarc
+test -e ~/.novarc && source ~/.novarc
+test -e ~/.ostackrc && source ~/.ostackrc
+
 # === Parameters 
 # 
+# HAVE TO CHANGE! ######
+
+# If you have not set the parameters via the openStack RC files, you can do it here
+## You don't need any of the below if you have your environment set up using
+# the standard OpenStack environment variables in novarc or .ostackrc.
+# Just defaults, IF you want.
+
 #export OS_PROJECT_NAME=eu-de
 #export OS_USERNAME="NUMBER OTC000....."
 #export OS_CACERT=/path/to/cacrt.pem
@@ -11,32 +34,12 @@
 #export S3_ACCESS_KEY_ID=S3 KEY
 #export S3_SECRET_ACCESS_KEY=S3 SECRET
 
-# === Variables
-#
-# === Examples
-# 
-# === Authors
-# 
-# Zsolt Nagy <Z.Nagy@t-systems.com>
-#
-# === Copyright
-#
-# Copyright 2016 T-Systems International GmbH
-#
-
-# USER SPECIFIC SETTINGS #################################################################
-
-# HAVE TO CHANGE! ######
-
-# NEEDED FOR TOKEN AUTH
-test -e ~/novarc && source ~/novarc
-test -e ~/.ostackrc && source ~/.ostackrc
-
 # These variables are in openstack environment format ...
-export OS_PROJECT_NAME="eu-de"
 #export OS_USERNAME="$OS_USERNAME"
 #export OS_PASSWORD="$OS_PASSWORD"
+# Those two don't change for OTC in Europe region ...
 export OS_USER_DOMAIN_NAME="${OS_USERNAME##* }"
+export OS_PROJECT_NAME="eu-de"
 
 #export S3_ACCESS_KEY_ID=XXXXXXXXXXXXXXXXXXXX
 #export S3_SECRET_ACCESS_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -50,14 +53,17 @@ export OS_USER_DOMAIN_NAME="${OS_USERNAME##* }"
 
 # Defaults ( override from command line )
 
+# Default VOLUMETYPE: SATA, SAS or SSD
 export VOLUMETYPE="SATA"
-export AZ="eu-de-01"
+# Leave AZ empty to derive from subnet's AZ (recommended)
+#export AZ="eu-de-01"
 
+# Set defaults if you always use the same settings ...
 export SECUGROUPNAME="default"
 export VPCNAME="vpc-0"
 export SUBNETNAME="subnet-1"
 export IMAGENAME="Standard_openSUSE_42.1_JeOS_latest"
-export NUMCOUNT=1
+#export NUMCOUNT=1
 export INSTANCE_TYPE="computev1-1"
 export INSTANCE_NAME="otcVM-$$"
 
@@ -71,5 +77,6 @@ export CREATE_ECS_WITH_PUBLIC_IP="false"
 
 export ECSACTIONTYPE="HARD"
 export WAIT_CREATE="true"
+#export BANDWIDTH=25
 ##########################################################################################
 
